340e60f133e0d52bf732b9d0ced1db7d96135217
Sean C <47064367+sean7391@users.noreply.github.com>
2020-08-17 13:54:22 -0400

    Update shuffle.cpp

--- 2017-18/December/shuffle.cpp
+++ 2017-18/December/shuffle.cpp
@@ -26,7 +26,7 @@ typedef pair<int, int> pii;
 const int dx[4]= {-1, 1, 0, 0}, dy[4] = {0, 0, -1, 1};
 
 void setIO(string name){
-	ios_base::sync_with_stdio(0); cin.tie(0);
+    ios_base::sync_with_stdio(0); cin.tie(0);
 	freopen((name + ".in").c_str(), "r", stdin);
 	freopen((name + ".out").c_str(), "w", stdout);
 }
