29e2cbb9c210a5d15f07ced412488e55ae324405
Sean C <47064367+sean7391@users.noreply.github.com>
2020-08-18 16:43:23 -0400

    Update mootube.cpp

--- 2017-18/January/mootube.cpp
+++ 2017-18/January/mootube.cpp
@@ -26,9 +26,9 @@ typedef pair<int, int> pii;
 const int dx[4]= {-1, 1, 0, 0}, dy[4] = {0, 0, -1, 1};
  
 void setIO(string name){
-	ios_base::sync_with_stdio(0); cin.tie(0);
-	freopen((name + ".in").c_str(), "r", stdin);
-	freopen((name + ".out").c_str(), "w", stdout);
+    ios_base::sync_with_stdio(0); cin.tie(0);
+    freopen((name + ".in").c_str(), "r", stdin);
+    freopen((name + ".out").c_str(), "w", stdout);
 }
  
 struct Edge{
