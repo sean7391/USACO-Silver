de8ed45a4768dc4c9b98a2629dad0a1b83b47d52
Sean C <47064367+sean7391@users.noreply.github.com>
2021-01-20 18:50:13 -0500

    Update measurement.cpp

--- 2017-18/December/measurement.cpp
+++ 2017-18/December/measurement.cpp
@@ -20,9 +20,9 @@ typedef long long ll;
 typedef pair<int, int> pii; 
 
 void setIO(string name){
-	ios_base::sync_with_stdio(0); cin.tie(0);
-	freopen((name + ".in").c_str(), "r", stdin);
-	freopen((name + ".out").c_str(), "w", stdout);
+    ios_base::sync_with_stdio(0); cin.tie(0);
+    freopen((name + ".in").c_str(), "r", stdin);
+    freopen((name + ".out").c_str(), "w", stdout);
 }
 
 struct measurement{
